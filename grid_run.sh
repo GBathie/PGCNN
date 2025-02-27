22c51dd4d2172d4c77ce044e96416fda71877263
GBathie <gbathie.dev@gmail.com>
2025-02-27 16:39:18 -0500

    Update script

--- grid_run.sh
+++ grid_run.sh
@@ -4,6 +4,8 @@ outfile=~/job_$OAR_JOB_ID.csv
 
 echo "Outputting to ${outfile}"
 
+uv venv
+source .venv/bin/activate
 uv pip install -r requirements.txt
 
-parallel -j1 --joblog ./parallel.log --ssh oarsh --slf $OAR_NODEFILE "cd PGCNN && python3 data_efficiency.py" ::: {1..10} > ${outfile}
+parallel -j1 --joblog ./parallel.log --ssh oarsh --slf $OAR_NODEFILE "cd PGCNN && source .venv/bin/activate && python3 data_efficiency.py" ::: {1..10} > ${outfile}
