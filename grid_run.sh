f6530b51aae64aec84e72c907435c34e65c38a85
Gabriel Bathie <gbathie@fnancy>
2025-02-27 22:39:27 +0100

    Scripts

--- grid_run.sh
+++ grid_run.sh
@@ -4,6 +4,6 @@ outfile=~/job_$OAR_JOB_ID.csv
 
 echo "Outputting to ${outfile}"
 
-uv pip install -r requirements.txt
+uv pip install -r requirements.txt --system
 
 parallel -j1 --joblog ./parallel.log --ssh oarsh --slf $OAR_NODEFILE "cd PGCNN && python3 data_efficiency.py" ::: {1..10} > ${outfile}
